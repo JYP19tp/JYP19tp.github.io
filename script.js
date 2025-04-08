7608ad21604a9a993320530ecb598053afbcbe80
JYP19tp <164713387+JYP19tp@users.noreply.github.com>
2025-04-08 17:29:09 +0900

    ナビゲーションバー追加
    -html
    section分類作成
    -css
    基本デザイン入力
    -JavaScript
    スクロールバーの機能

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("フォートポリオウェブページへようこそ！");
+
+document.querySelectorAll(`nav ul li a`).forEach(anchor => {
+    anchor.addEventListener(`click`, function (e) {
+        e.preventDefault();  // 基本動作を防ぐ(リンククリック等)
+
+        // クリックしたリンクのhref属性に該当するidへ柔らかく移動
+        document.querySelector(this.getAttribute(`href`)).scrollIntoView({
+            behavior: `smooth`,
+            block: `start`
+        });
+    });
+});
